8695554d7e23b8efbb4e485a768a116e5d4ae8c6
tobi michael <tobimichael65@gmail.com>
2021-08-24 14:57:59 +0100

    new3

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import "fmt"
+type Person struct {
+	Age     int
+	name    string
+	Address string
+}
 
 func main() {
 	fmt.Println("na me be this")
 	strings()
 	inter()
 
-n, a :=	mich("michael", 500)
-fmt.Println(n, a)
+	n, a := mich("michael", 500)
+	fmt.Println(n, a)
 	H := f(15, 12, 4)
 	fmt.Println(H)
 
 	FutureAge := calculateAge(15, 13)
-	fmt.Println(FutureAge )
+	fmt.Println(FutureAge)
 
 	arrays()
+
+	populationlist:= []int{60, 46, 65, 53}
+	t:=totalPopulation(populationlist)
+	fmt.Println(t )
+
 
 }
 
@@ -40,14 +50,34 @@ func f(a int, b int, c int) int {
 	return d
 }
 
-func calculateAge(currentAge , numberOfYears int ) int {
+func calculateAge(currentAge, numberOfYears int) int {
 	futureAge := currentAge + numberOfYears
 	return futureAge
 }
 
 func arrays() {
-	arra := []int{42, 56, 38,49}
-	for	idx, val := range arra {
+	arra := []int{42, 56, 38, 49}
+	for idx, val := range arra {
 		fmt.Println(idx, val)
 	}
+
+	whyy := []string{"mr", "Niger", "Cad"}
+	for idx, val := range whyy {
+		fmt.Println(idx, val)
+	}
+
+	tobi := Person{
+	Address:"lavender",
+	Age : 3448983,
+	}
+	fmt.Println(tobi)
+}
+
+func totalPopulation(listOfPopulation []int) int {
+	total :=0
+	for _, value := range listOfPopulation {
+		fmt.Println(total, value)
+		total = total + value
+	}
+	return  total
 }
